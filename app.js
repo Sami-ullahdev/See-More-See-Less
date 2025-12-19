6d4638fdcbd9666e22ee7360800c3da182da02ff
samiullahDeveloper786 <samiullahdeveloperr@gmail.com>
2025-12-20 00:29:21 +0500

    Final Commit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const shortText = paragraph.innerHTML;
 
 let expandpara = "Lorem ipsum, dolor sit amet consectetur adipisicing elit.Officia necessitatibus sint dolor sit amet consectetur adipisicing elit.Officia necessitatibus sint et libero minus, consequuntur incidunt.";
 
-function expand(){
-    if(btn.innerHTML.toLowerCase() == "see more"){
+function expand() {
+    if (btn.innerHTML.toLowerCase() == "see more") {
         paragraph.innerHTML = expandpara
         btn.innerHTML = "See Less"
     } else {
